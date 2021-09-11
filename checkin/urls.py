eef66f776d3927ef276454383b5afc6f5cda9674
Cameron Bowers <cameronbowers0@gmail.com>
2021-09-11 11:33:07 -0700

    Partial integration of login system. Revert successful.

--- checkin/urls.py
+++ checkin/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('form/', views.student_contact, name='checkin-student_contact'),
     path('about/', views.about, name='checkin-about'),
     path('', views.home, name='checkin-home'),
-    path('data', views.data, name='checkin-data')
+    path('data/', views.data, name='checkin-data')
 ]
