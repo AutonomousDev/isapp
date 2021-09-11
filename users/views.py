f45959ddf85a6749d9fd2a13e617176a713d5422
Cameron Bowers <cameronbowers0@gmail.com>
2021-09-11 11:56:52 -0700

    Login, logout and profile are working. Profile is incomplete.

--- users/views.py
+++ users/views.py
@@ -9,9 +9,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('checkin-home')
+            messages.success(request, f'Account created for {username}!, Please log in.')
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+def profile(request):
+    return render(request, 'users/profile.html')
