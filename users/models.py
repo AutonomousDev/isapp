eeda525bbf1d1df9baaf9f6fd28d3017a031138b
Cameron Bowers <cameronbowers0@gmail.com>
2021-09-20 09:36:02 -0700

    Added user profiles to the user DB model.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.Model):
+    """Extended the built in user model"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.JPG', upload_to='profile_pics')
+
+    def __str__(self):
+        """displays fstring when user is returned"""
+        return f'{self.user.username} Profile'
+
