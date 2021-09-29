6b7e68ec9bcc1cbab6f0cbd595f843c517c30a9b
Cameron Bowers <cameronbowers0@gmail.com>
2021-09-29 09:35:16 -0700

    Fixed meeting form success messages. Needed to use super(), not redirect.

--- checkin/views.py
+++ checkin/views.py
@@ -8,6 +8,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 
 # Create your views here.
 def student_contact(request):
+    """Depreciated use StudentMeetingView. TODO Phase out"""
     context = {
         'students': Student.objects.all()
     }
@@ -25,12 +26,6 @@ def home(request):
 class Data(generic.ListView):
     model = Student
 
-# def data(request):
-#     context = {
-#         'meetings': StudentMeeting.objects.all()
-#     }
-#     return render(request, 'checkin/data_summary.html', context)
-
 
 class StudentDetailView(generic.DetailView):
     model = Student
@@ -40,15 +35,20 @@ class StudentMeetingView(SuccessMessageMixin, generic.CreateView):
     model = models.StudentMeeting
     form_class = forms.StudentMeetingForm
     template_name = 'checkin/student_meeting_form.html'
-    success_message = f'Meeting recorded'
-    success_url = 'checkin-student-meeting'
+    success_url = '/student_meeting/'
+    success_message = "%(student)s's Meeting recorded"
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            this_student=self.object.student,
+        )
 
     def form_valid(self, form):
         student_meeting = form.save(commit=False)
         student_meeting.instructor = self.request.user
         student_meeting.save()
-
-        return redirect('checkin-student-meeting')
+        return super().form_valid(form)
 
 
 
