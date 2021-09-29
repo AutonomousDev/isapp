1e2d8be8e03093823cb67cc00222d6dc038eb2ab
Cameron Bowers <cameronbowers0@gmail.com>
2021-09-29 09:00:51 -0700

    Added login_required to URLS.py to prevent non-user access.

--- checkin/urls.py
+++ checkin/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
     path('form/', views.student_contact, name='checkin-student_contact'),
     path('about/', views.about, name='checkin-about'),
     path('', views.home, name='checkin-home'),
-    path('data/', views.Data.as_view(), name='checkin-data'),
-    path('student/<int:pk>', views.StudentDetailView.as_view(), name='checkin-student-detail'),
-    path('student_meeting/', views.StudentMeetingView.as_view(), name='checkin-student-meeting'),
+    path('data/', login_required(views.Data.as_view()), name='checkin-data'),
+    path('student/<int:pk>', login_required(views.StudentDetailView.as_view()), name='checkin-student-detail'),
+    path('student_meeting/', login_required(views.StudentMeetingView.as_view()), name='checkin-student-meeting'),
 
 ]
