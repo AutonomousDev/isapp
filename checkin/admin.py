0217b8cf53b00b4d260030892e4205cf7860b829
Cameron Bowers <cameronbowers0@gmail.com>
2021-10-06 15:47:35 -0700

    Student enrollment is now managed by enrollment models.
    meetings don't track courses anymore.
    
    Meeting week view now uses the new enrollment model instead of storing enrollment on meetings.

--- checkin/admin.py
+++ checkin/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Course, CourseProvider, Student, StudentMeeting
+from .models import Course, CourseProvider, Student, StudentMeeting, CourseEnrollment
 
 admin.site.register(Course)
 admin.site.register(CourseProvider)
 admin.site.register(Student)
 admin.site.register(StudentMeeting)
+admin.site.register(CourseEnrollment)
 
--- checkin/models.py
+++ checkin/models.py
@@ -6,6 +6,17 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+class CourseProvider(models.Model):
+    """Model representing course providers"""
+    provider_name = models.CharField(max_length=200, help_text="Enter the course provider name.")
+    provider_website = models.CharField(max_length=255, help_text="Enter the course providers website.", blank=True, default='')
+    lms = models.CharField(max_length=610, help_text="Enter the learning management systems web address", blank=True, default='')
+
+    def __str__(self):
+        """String representing the Model object"""
+        return self.provider_name
+
+
 class Course(models.Model):
     """Model representing courses"""
     course_name = models.CharField(max_length=200, help_text="Enter the name of the course.")
@@ -20,21 +31,12 @@ class Course(models.Model):
         return f'{self.course_name}'
 
 
-class CourseProvider(models.Model):
-    """Model representing course providers"""
-    provider_name = models.CharField(max_length=200, help_text="Enter the course provider name.")
-    provider_website = models.CharField(max_length=255, help_text="Enter the course providers website.", blank=True, default='')
-    lms = models.CharField(max_length=610, help_text="Enter the learning management systems web address", blank=True, default='')
-
-    def __str__(self):
-        """String representing the Model object"""
-        return self.provider_name
-
-
 class Student(models.Model):
     """Model representing a student"""
     first_name = models.CharField(max_length=50, help_text="Student first name.")
     last_name = models.CharField(max_length=50, help_text="Student last name.")
+    id_number = models.CharField(max_length=10, help_text="Student ID number")
+    active = models.BooleanField(help_text="Uncheck box for inactive students", default=True)
     course = models.ManyToManyField(Course, help_text="Select courses student is enrolled in.", blank=True, default='')
 
     def __str__(self):
@@ -46,6 +48,19 @@ class Student(models.Model):
         return reverse("checkin-student-detail", args=[str(self.id)])
 
 
+class CourseEnrollment(models.Model):
+    """This model tracks what courses a student is enrolled in, start dats and end date"""
+    student = models.ForeignKey(Student, help_text="Student Name.", on_delete=models.CASCADE, null=True)
+    course = models.ForeignKey(Course, help_text="Select course student is enrolled in.", blank=True, default='', on_delete=models.PROTECT)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+
+    def __str__(self):
+        """String representing the Model object"""
+        return str(self.student) + ": " + str(self.course)
+
+
 class StudentMeeting(models.Model):
     """Model representing a check-in appointment"""
 
@@ -55,7 +70,6 @@ class StudentMeeting(models.Model):
     attended_meeting = models.BooleanField()
     missing_work_amount = models.IntegerField(default=0)
     narrative = models.TextField(blank=True, default='')
-    current_courses = models.ManyToManyField(Course, blank=True, default='')
 
     def __str__(self):
         """String representing the Model object"""
