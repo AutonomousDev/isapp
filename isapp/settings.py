80c33a878139087b2b6a0cf5ba3696061b31d37b
Cameron Bowers <cameronbowers0@gmail.com>
2021-11-02 22:10:36 -0700

    Allowed host is buggin

--- isapp/settings.py
+++ isapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i)^ysia&pb)*r&z0&k$@is#(1w1@p8aw9d-%qxzy-k$q&rs-61
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["is-app.herokuapp.com", "127.0.0.1", "127.0.0.1:8000"]
+ALLOWED_HOSTS = ["*", "is-app.herokuapp.com", "127.0.0.1", "127.0.0.1:8000"]
 
 
 # Application definition
