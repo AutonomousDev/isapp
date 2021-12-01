8ce855e66fb781299eb30254addbd9b990c7736b
Cameron Bowers <58051980+AutonomousDev@users.noreply.github.com>
2021-12-01 14:12:14 -0800

    Changed Redis hosting from local host to server based.

--- isapp/settings.py
+++ isapp/settings.py
@@ -163,12 +163,14 @@ LOGIN_URL = 'login'
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-        "LOCATION": "unique-snowflake",
-    },
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get('REDIS_URL'),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
     "select2": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
+        "LOCATION": os.environ.get('REDIS_URL')+"/2",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
