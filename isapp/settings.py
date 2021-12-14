718bae5b892594bef4bcae1163a26c8fda719bdc
Cameron Bowers <cameronbowers0@gmail.com>
2021-12-13 21:24:29 -0800

    Resolved "Server Error (500) with DEBUG=False" bug
    solution was to run,
    
    python manage.py collectstatic

--- isapp/settings.py
+++ isapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*", "is-app.herokuapp.com", "127.0.0.1", "127.0.0.1:8000"]
+ALLOWED_HOSTS = ["is-app.herokuapp.com", "127.0.0.1", "127.0.0.1:8000"]
 
 # Makes Error messages red
 from django.contrib.messages import constants as messages
