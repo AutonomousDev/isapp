c772aa5e026c66906220fc8aa621a3f6419dc50a
Cameron Bowers <cameronbowers0@gmail.com>
2021-12-21 17:52:21 -0800

    Maybe fixed profile creation bug

--- users/models.py
+++ users/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 # Create your models here.
@@ -16,13 +14,3 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
 
-# TODO Clean this up
-@receiver(post_save, sender=User)  # Auto creates a profile for each new user
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)  # Auto creates a profile for each new user
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
