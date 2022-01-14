7c6f19a48da1e27f6d8f4daab80a0ce1927ac425
Cameron Bowers <cameronbowers0@gmail.com>
2022-01-14 11:01:58 -0800

    Announcements view partially added.

--- announcements/urls.py
+++ announcements/urls.py
@@ -4,10 +4,16 @@ import datetime
 from .views import (
     PostListView,
     PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
 )
 
 urlpatterns = [
     path('', PostListView.as_view(), name='check_in-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
     ]
