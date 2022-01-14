f1c9d3f88b121f5d58945bbdc3c462fe329c3983
Cameron Bowers <cameronbowers0@gmail.com>
2022-01-14 09:42:45 -0800

    Created announcement app. Added app to installed apps list. Added announcements urls to the main urls.

--- isapp/urls.py
+++ isapp/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('check_in/', include('check_in.urls')),
     path('', include('check_in.urls')),
+    path('', include('announcements.urls')),
     path('register/', login_required(user_views.register), name='register'),  # Public account creation is not allowed
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
